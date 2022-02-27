42f68e07bae17875c6259cbd86525189574e2084
Saad Amin <saadvi@gmail.com>
2022-02-27 22:19:19 +0000

    first test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
+import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import ShowTime from './ShowTime';
 
 function App() {
+
+const imgClick = () => {
+  setTime(getTime());
+ console.log(currentTime);
+  }
+  const getTime = () =>{
+    var d = new Date(); return d.toLocaleString();  
+  }
+  const [currentTime, setTime] = useState(getTime())
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+
+        <a id="clickToshowTime" onClick={imgClick} href="#">
+        <img src={logo} 
+         className="App-logo" alt="logo" />
         </a>
+        <ShowTime
+        getTime={getTime}
+        />
       </header>
     </div>
   );
---
+++ src/ShowTime.js
@@ -0,0 +1,7 @@
+const ShowTime = ({ getTime }) => {
+    return (
+    <p>Current Time : {getTime()}</p>
+    )
+}
+
+export default ShowTime;
